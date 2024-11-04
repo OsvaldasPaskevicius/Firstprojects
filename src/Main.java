91acecd8b2c903eb76c8567c260ff0bad68f2c56
OsvaldasPaskevicius <emigrantas0202@icloud.com>
2024-11-04 17:38:54 +0200

    PirmasDarbas

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        byte numberOne = 50;
+        short shortOne = 250;
+        int intOne = 200;
+        long longOne = 50000;
+        System.out.println((numberOne + shortOne + intOne * 10) + longOne);
+    }
+}
