0356a6377b5e7e55b649dd09151b2ec71a3b4147
OsvaldasPaskevicius <emigrantas0202@icloud.com>
2024-11-04 19:34:49 +0200

    main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        byte numberOne = 50;
-        short shortOne = 250;
-        int intOne = 200;
-        long longOne = 50000;
-        System.out.println((numberOne + shortOne + intOne * 10) + longOne);
-
-
-
-        double pounds = 20;
-        double kilograms = pounds * 0.45359237;
-        System.out.println(pounds + " Lbs = " + kilograms + " Kg");
+        char mySimpleChar = '?';
+        char myUnicodeChar = '\u003F';
+        char myDecimalChar = 63;
+        System.out.println("My values are :" + mySimpleChar + myUnicodeChar + myDecimalChar);
     }
 }
