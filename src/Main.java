d64ee7b5efbeb974e548e4cd9dec8fe8c59eeee7
OsvaldasPaskevicius <emigrantas0202@icloud.com>
2024-11-04 18:04:45 +0200

    AntrasDarbas

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,11 @@ public class Main {
         int intOne = 200;
         long longOne = 50000;
         System.out.println((numberOne + shortOne + intOne * 10) + longOne);
-        System.out.println("Testuojam commit");
+
+
+
+        double pounds = 20;
+        double kilograms = pounds * 0.45359237;
+        System.out.println(pounds + " Lbs = " + kilograms + " Kg");
     }
 }
